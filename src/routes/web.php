0a05db57494e5ddbb41c545e43caa31b14752d2a
tomoe <tm.717-ktsshm@au.com>
2024-06-02 16:44:05 +0900

    お問い合わせフォーム作成完了

--- src/routes/web.php
+++ src/routes/web.php
@@ -16,4 +16,6 @@ use App\Http\Controllers\ContactController;
 
 Route::get('/', [ContactController::class, 'index']);
 
+Route::POST('/contacts/confirm', [ContactController::class, 'confirm']);
 
+Route::post('/contacts', [ContactController::class, 'store']);
